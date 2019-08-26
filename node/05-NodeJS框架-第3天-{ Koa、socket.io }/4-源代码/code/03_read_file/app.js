4b4e35d2ecdb3a8a44cdd83d31bbf5e73b4c9514
幕启 <34263338+OnionGF@users.noreply.github.com>
2019-08-26 16:26:47 +0800

    chore: node ssr

--- node/05-NodeJS框架-第3天-{ Koa、socket.io }/4-源代码/code/03_read_file/app.js
+++ node/05-NodeJS框架-第3天-{ Koa、socket.io }/4-源代码/code/03_read_file/app.js
@@ -5,9 +5,9 @@ const Koa = require('koa');
 let app = new Koa();
 
 
-function asyncReadFile() {
+function asyncReadFile(url) {
    return new Promise(function(resolve,reject) {
-      fs.readFile('./index.html',(err,data)=>{
+      fs.readFile(url,(err,data)=>{
         // 1:失败，err  2：成功需要data
           if(err) {
              reject(err);
@@ -23,21 +23,20 @@ function asyncReadFile() {
 
 // aysnc(声明函数中有异步操作) + await(等待) = promise (三合一)
 app.use(async (ctx)=>{
+  // 设置响应头
+  ctx.set('content-type','text/html;charset=utf-8');
   if(ctx.url === '/') {
-    console.log('haha')
     // 响应首页
-    let data = await asyncReadFile();  // 异常使用catch
-    console.log(data);
-
-    // 设置响应头
-    ctx.set('content-type','text/html;charset=utf-8');
-
-
-    ctx.body = data;
+    ctx.body = await asyncReadFile('./index.html');
+  }else if(ctx.url === '/content') {
+    // 响应首页
+    ctx.body = await asyncReadFile('./second.html');
   } else {
     // ok
       ctx.body = 'ok';  // 二进制数据buffer 作为下载
   }
 });
 
-app.listen(8888);
+app.listen(8888,()=>{
+  console.log('项目启动成功...')
+});
